075ebad4d1a17d09c1a38a70a42f7130b4819fda
nikhiltank1983 <86612921+nikhiltank1983@users.noreply.github.com>
2021-06-28 16:04:11 -0400

    Update rds_tf.tf

--- rds_tf/rds_tf.tf
+++ rds_tf/rds_tf.tf
@@ -12,7 +12,7 @@ resource "aws_db_instance" "default" {
   username             = "foo"
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
-  db_subnet_group_name = rds-alphabeta-subnetgroup
+  db_subnet_group_name = "rds-alphabeta-subnetgroup"
   vpc_security_group_ids = ["sg-ad15addb" ]
   skip_final_snapshot  = true
   multi_az             = false
