a78b72dcb0d5572c9d141ede36ea91783c91df8d
nikhiltank1983 <86612921+nikhiltank1983@users.noreply.github.com>
2021-06-28 15:45:45 -0400

    Add files via upload

---
+++ rds_tf/rds_tf.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+version = "2.69.0"
+region="us-east-1"
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t3.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  db_subnet_group_name = rds-alphabeta-subnetgroup
+  vpc_security_group_ids = [ sg-ad15addb ]
+  skip_final_snapshot  = true
+}
