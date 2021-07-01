850b16cdc02d33972371e793796bc511bff74fd1
nikhiltank1983 <86612921+nikhiltank1983@users.noreply.github.com>
2021-07-01 11:19:27 -0400

    Update rds_tf.tf

--- rds_tf/rds_tf.tf
+++ rds_tf/rds_tf.tf
@@ -3,14 +3,22 @@ version = "2.69.0"
 region="us-east-1"
 }
 
+variable "username" {
+  description = "DB username"
+}
+
+variable "password" {
+  description = "DB password"
+}
+
 resource "aws_db_instance" "default" {
   allocated_storage    = 10
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
   name                 = "mydb"
-  username             = "foo"
-  password             = "foobarbaz"
+  username             = "${var.username}"
+  password             = "${var.password}"
   parameter_group_name = "default.mysql5.7"
   db_subnet_group_name = "rds-alphabeta-subnetgroup"
   vpc_security_group_ids = ["sg-ad15addb" ]
